229454643cd6fd89a302a41cfe5001c6f7a132a7
svaibavbajra <svaibavbajra@gmail.com>
2021-06-19 17:38:22 +0530

    c1

--- src/components/mobilenavbar.jsx
+++ src/components/mobilenavbar.jsx
@@ -7,12 +7,11 @@ function MobileNavbar(){
 
 	return(
 	<div>
-		<div style={{position:"fixed", top:"49vh", right:"0"}} onClick={() => menuClicked(true)}>
-			menu
+		<div style={{position:"fixed", top:"49vh", right:"0"}} onClick={() => menuClicked(!menu)}>
+			{menu?<div>menu</div>:<div>close</div>}
 		</div>
 
-		{menu?<div style={{backgroundColor:"white", zIndex:"2", position:"fixed", top:"0", right:"0", height:"100vh", width:"100vw"}}>
-			<div onClick={() => menuClicked(false)}>close</div>
+		{menu?<div style={{backgroundColor:"white", position:"fixed", top:"0", right:"0", height:"100vh", width:"100vw"}}>
 			<NavLink className="nav-link" exact to="/home">Home</NavLink>
 	        <NavLink className="nav-link" exact to="/projects">Projects</NavLink>
 	        <NavLink className="nav-link" exact to="/services">Services</NavLink>
