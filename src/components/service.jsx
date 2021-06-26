dba2905e491b7142d31567164ed5aae9930d9a3b
svaibavbajra <svaibavbajra@gmail.com>
2021-06-26 10:59:11 +0530

    lander

--- src/components/service.jsx
+++ src/components/service.jsx
@@ -18,7 +18,7 @@ function Service(){
 					<div className="container">
 			 			<div className="row">
 			 				<div className="col-md-6">
-			 					An image goes here
+			 					<iframe src="https://ntmaker.gfto.ru/newneontexten/?image_height=200&image_width=600&image_font_shadow_width=30&image_font_size=100&image_background_color=003B49&image_text_color=FF91A9&image_font_shadow_color=F7406B&image_url=&image_text=Services&image_font_family=Nickainley&" frameborder='no' scrolling='no' width="600" height="200"></iframe>
 			 				</div>
 			 				<div className="col-md-6" style={{textAlign:"left"}}>
 			 					<h2>
@@ -35,22 +35,22 @@ function Service(){
 				 	<div className="container" style={{marginTop:"2em"}}>
 			 			<div className="row">
 			 				<div className="col-md-4 col-sm-6">
-			 					<WorkCard />
+			 					<WorkCard title="Web Design"/>
 			 				</div>
 			 				<div className="col-md-4 col-sm-6">
-			 					<WorkCard />
+			 					<WorkCard title="Graphic Design"/>
 			 				</div>
 			 				<div className="col-md-4 col-sm-6">
-			 					<WorkCard />
+			 					<WorkCard title="Marketing"/>
 			 				</div>
 			 				<div className="col-md-4 col-sm-6">
-			 					<WorkCard />
+			 					<WorkCard title="Virtual Assistance"/>
 			 				</div>
 			 				<div className="col-md-4 col-sm-6">
-			 					<WorkCard />
+			 					<WorkCard title="Web Develoment"/>
 			 				</div>
 			 				<div className="col-md-4 col-sm-6">
-			 					<WorkCard />
+			 					<WorkCard title="Content Creation"/>
 			 				</div>
 			 			</div>
 			 		</div>
--- src/components/startproject.jsx
+++ src/components/startproject.jsx
@@ -5,7 +5,7 @@ function StartProject(){
 	const history=useHistory();
 
 	return(
-		<div style={{color:"white", position:"fixed", zIndex:"100", bottom:"35px", right:"35px"}}> 
+		<div style={{color:"white", position:"fixed", zIndex:"100", bottom:"35px", right:"35px", backgroundColor:"transparent"}}> 
 			<span className="buttons" onClick={()=>history.push("/contact")}>Start your Project</span>
 		</div>
 	)
