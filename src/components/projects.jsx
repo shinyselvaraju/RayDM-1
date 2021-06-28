fdd03d71cf926d54c17ed83f81471da100b555b5
svaibavbajra <svaibavbajra@gmail.com>
2021-06-28 22:11:53 +0530

    hover effects and new page

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -6,49 +6,58 @@ import {useEffect, useState} from "react";
 
 const works=[
 	{
-		img:"webdesign.jpg",
-		id: "webdesign",
-		url:""
+		img:"archer.jpeg",
+		title: "Web Development",
+		url:"",
+		id:1
+	},
+	{
+		img:"idiotic.jpeg",
+		title: "Web Development",
+		url:"",
+		id:2
+	},
+	{
+		img:"sumit.jpeg",
+		title: "Web Development",
+		url:"",
+		id:3
+	},
+	{
+		img:"topsea.jpeg",
+		title: "Web Development",
+		url:"",
+		id:4
+	},
+	{
+		img:"shine.jpeg",
+		title: "Web Development",
+		url:"",
+		id:5
 	},
 	{
 		img:"webdesign.jpg",
-		id: "webdesign",
-		url:""
+		title: "Graphic Design",
+		url:"",
+		id:6
 	},
 	{
 		img:"webdesign.jpg",
-		id: "webdev",
-		url:""
+		title: "Graphic Design",
+		url:"",
+		id:7
 	},
 	{
 		img:"webdesign.jpg",
-		id: "webdev",
-		url:""
+		title: "branding",
+		url:"",
+		id:8
 	},
 	{
 		img:"webdesign.jpg",
-		id: "ads",
-		url:""
-	},
-	{
-		img:"webdesign.jpg",
-		id: "ads",
-		url:""
-	},
-	{
-		img:"webdesign.jpg",
-		id: "ads",
-		url:""
-	},
-	{
-		img:"webdesign.jpg",
-		id: "branding",
-		url:""
-	},
-	{
-		img:"webdesign.jpg",
-		id: "branding",
-		url:""
+		title: "branding",
+		url:"",
+		id:9
 	}
 ]
 
@@ -56,6 +65,7 @@ function Projects(){
 
 	const [device, setDevice]=useState("laptop");
 	const [tablet, setTablet] = useState(false);
+	const [tile, setTile] = useState(0);
 
 	useEffect(() => {
 	  window.scrollTo(0, 0);
@@ -83,8 +93,14 @@ function Projects(){
 				</div>
 			</div>
 			<div className="container" style={{marginTop:"2em"}}>
-	 			<div className="row">
-	 				{works.map((work) =>(<div className="col-md-4 col-sm-6"><WorkCard /></div>))
+	 			<div className="row" id="webdev">
+	 				<h1>Web Development</h1>
+	 				{works.map((work) =>(work.title==="Web Development"?<div className="col-lg-4 col-md-6 col-sm-6" style={tile===work.id||tile===0?{}:{opacity:"0.3"}} onMouseOver={()=>setTile(work.id)} onMouseOut={()=>setTile(0)}><WorkCard img={work.img} title={work.title}/></div>:<div />))
+	 				}
+	 			</div>
+	 			<div className="row" id="graphicdes">
+	 				<h1>Graphic Design</h1>
+	 				{works.map((work) =>(work.title==="Graphic Design"?<div className="col-lg-4 col-md-6 col-sm-6" style={tile===work.id||tile===0?{}:{opacity:"0.3"}} onMouseOver={()=>setTile(work.id)} onMouseOut={()=>setTile(0)}><WorkCard img={work.img} title={work.title}/></div>:<div />))
 	 				}
 	 			</div>
 			 </div>
