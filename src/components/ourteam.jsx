e7b9bc0be5254400112a273655b6ade4a2ecc797
svaibavbajra <svaibavbajra@gmail.com>
2021-06-29 10:09:30 +0530

    removed heading on images

--- src/components/ourteam.jsx
+++ src/components/ourteam.jsx
@@ -35,47 +35,54 @@ function AboutUs(){
 					<img src="about.png" style={{maxWidth:"100vw"}}></img>
 				</div>
 		</div>
-		<div className="container" style={{color:"white"}}>
-			<h2>
+		<div className="container" style={{color:"white", marginBottom:"1em"}}>
+			<h2 style={{margin:"2em 0"}}>
 				Our Boring Story
 			</h2>
 			<p>
 				lorem ipsum Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed blandit massa vel mauris sollicitudin dignissim. Phasellus ultrices tellus eget ipsum ornare molesti
 				ipsum Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed blandit massa vel mauris sollicitudin dignissim. Phasellus ultrices tellus eget ipsum ornare molesti
 				ipsum Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed blandit massa vel mauris sollicitudin dignissim. Phasellus ultrices tellus eget ipsum ornare molesti 
+				<hr />
 			</p>
 		</div>
-		<div className="container">
+		<div className="container" style={{color:"white"}}>
+			<h1 style={{marginTop:"2em", marginBottom:"1em"}}>Meet our Team</h1>
 			<div className="row">
-				<div className="col-md-4 col-sm-6">
-					<img src={"webdesign.jpg"} style={{height:"100px", width:"100px", maxHeight:"25vh", borderRadius:"100%"}}/>
-					<h3 className="Heading">Bajra</h3>
-					<p>CEO, Web Developer</p>
+				<div className="col-lg-4 col-md-6 col-sm-6" style={{margin:"1em 0"}}>
+					<img src={"webdesign.jpg"} style={{height:"100%", width:"100%", maxHeight:"35vh"}}/>
+					<h3 className="Heading">Shiny Selvaraju</h3>
+					<p>Lead Web Developer and loves meeting new people</p>
 				</div>
-				<div className="col-md-4 col-sm-6">
-					<img src={"webdesign.jpg"} style={{height:"100%", width:"100%", maxHeight:"25vh", borderRadius:"150px"}}/>
+				<div className="col-lg-4 col-md-6 col-sm-6" style={{margin:"1em 0"}}>
+					<img src={"webdesign.jpg"} style={{height:"100%", width:"100%", maxHeight:"35vh"}}/>
+					<h3 className="Heading">Charan</h3>
+					<p>Marketing and Instagram Strategist while enjoying his cuppa tea</p>
+				</div>
+				<div className="col-lg-4 col-md-6 col-sm-6" style={{margin:"1em 0"}}>
+					<img src={"webdesign.jpg"} style={{height:"100%", width:"100%", maxHeight:"35vh"}}/>
+					<h3 className="Heading">Vasantha</h3>
+					<p>Graphic designer from NIFT and wants anything japanese or anime</p>
+				</div>
+				<div className="col-lg-4 col-md-6 col-sm-6" style={{margin:"1em 0"}}>
+					<img src={"webdesign.jpg"} style={{height:"100%", width:"100%", maxHeight:"35vh"}}/>
 					<h3 className="Heading">Bajra</h3>
-					<p>CEO, Web Developer</p>
-				</div><div className="col-md-4 col-sm-6">
-					<img src={"webdesign.jpg"} style={{height:"100%", width:"100%", maxHeight:"25vh", borderRadius:"150px"}}/>
-					<h3 className="Heading">Bajra</h3>
-					<p>CEO, Web Developer</p>
-				</div><div className="col-md-4 col-sm-6">
-					<img src={"webdesign.jpg"} style={{height:"100%", width:"100%", maxHeight:"25vh", borderRadius:"150px"}}/>
-					<h3 className="Heading">Bajra</h3>
-					<p>CEO, Web Developer</p>
-				</div><div className="col-md-4 col-sm-6">
-					<img src={"webdesign.jpg"} style={{height:"100%", width:"100%", maxHeight:"25vh", borderRadius:"150px"}}/>
-					<h3 className="Heading">Bajra</h3>
-					<p>CEO, Web Developer</p>
-				</div><div className="col-md-4 col-sm-6">
-					<img src={"webdesign.jpg"} style={{height:"100%", width:"100%", maxHeight:"25vh", borderRadius:"150px"}}/>
-					<h3 className="Heading">Bajra</h3>
-					<p>CEO, Web Developer</p>
-				</div><div className="col-md-4 col-sm-6">
-					<img src={"webdesign.jpg"} style={{height:"100%", width:"100%", maxHeight:"25vh", borderRadius:"150px"}}/>
-					<h3 className="Heading">Bajra</h3>
-					<p>CEO, Web Developer</p>
+					<p>Web Developer and a random dood from the moon</p>
+				</div>
+				<div className="col-lg-4 col-md-6 col-sm-6" style={{margin:"1em 0"}}>
+					<img src={"webdesign.jpg"} style={{height:"100%", width:"100%", maxHeight:"35vh"}}/>
+					<h3 className="Heading">Sagar</h3>
+					<p>Graphic designer and video editor from the land of rajputs</p>
+				</div>
+				<div className="col-lg-4 col-md-6 col-sm-6" style={{margin:"1em 0"}}>
+					<img src={"webdesign.jpg"} style={{height:"100%", width:"100%", maxHeight:"35vh"}}/>
+					<h3 className="Heading">Rishab</h3>
+					<p>Social media Manager *awesome bgm intensifies*</p>
+				</div>
+				<div className="col-lg-4 col-md-6 col-sm-6" style={{margin:"1em 0"}}>
+					<img src={"webdesign.jpg"} style={{height:"100%", width:"100%", maxHeight:"35vh"}}/>
+					<h3 className="Heading">Ariyana</h3>
+					<p>Social media Manager *hello i am ariyana*</p>
 				</div>
 			</div>
 		</div>
